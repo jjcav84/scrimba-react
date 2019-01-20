28098a218fc9b39bcc6e325482dde95f202e051f
Jacob Cavazos <jjcav84@gmail.com>
2019-01-19 23:28:59 -0600

    edited text for main, footer, and header components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react"
 function Navbar() {
   return (
     <header>
-      <h3>This is the header</h3>
+      This is the header
     </header>
   )
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -2,15 +2,9 @@ import React from "react"
 
 function MainContent() {
   return (
-    <div>
-      <h1>Jacob Cavazos</h1>
-      <p>This is a paragraph about me...</p>
-      <ul>
-        <li>Japan</li>
-        <li>Turkey</li>
-        <li>Nordic Countries</li>
-      </ul>
-    </div>
+    <main>
+     This is the main section
+    </main>
   )
 }
 
