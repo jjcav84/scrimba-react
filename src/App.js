ba29dc5e18833138a64ae08ab5c2c1a2f07a25e4
Jacob Cavazos <jjcav84@gmail.com>
2019-01-19 22:24:51 -0600

    changed Navbar component to Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-import Navbar from "./components/Navbar"
+import Header from "./components/Header"
 import MainContent from "./components/MainContent"
 import Footer from "./components/Footer"
 
 function App() {
     return (
         <div>
-          <Navbar />
+          <Header />
           <MainContent />
           <Footer />
         </div>  
