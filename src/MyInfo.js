0e3ab5101d21cab8fa81d321612d878a9f25d4e7
Jacob Cavazos <jjcav84@gmail.com>
2019-01-19 20:11:36 -0600

    created separate MyInfo component and got it working properly with index.js

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Jacob Cavazos</h1>
+      <p>This is a paragraph about me...</p>
+      <ul>
+        <li>Japan</li>
+        <li>Turkey</li>
+        <li>Nordic Countries</li>
+      </ul>
+    </div>
+  )
+}
+
+export default MyInfo
---
+++ tmp/dev-server/react-static-browser-plugins.js
@@ -0,0 +1,12 @@
+// Imports
+
+
+// Plugins
+const plugins = [{
+  location: "/Users/jacobcavazos/React/scrimba-1",
+  plugins: [],
+  hooks: {}
+}]
+
+// Export em!
+export default plugins
