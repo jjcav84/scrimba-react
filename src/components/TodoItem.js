170660190ed8f99c4e15411c81c6c35debfbac2e
Jacob Cavazos <jjcav84@gmail.com>
2019-01-21 03:27:53 -0600

    added some css module sytling to the app

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function TodoItem() {
     return (
-        <div>
+        <div className="todo-item mx-auto my-1">
             <input type="checkbox" />
             <p>Placeholder text here</p>
         </div>
